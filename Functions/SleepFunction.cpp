bafea7449389bbef2957e72f34e2a69a340ecb86
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-05-26 20:42:11 +0300

    Add files via upload

---
+++ Functions/SleepFunction.cpp
@@ -0,0 +1,10 @@
+#include <unistd.h> //sleep library
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"The system will sleep for 10 seconds"<<endl;
+    sleep(10); //sleep for 10 seconds
+    cout<<"The system has slept for 10 seconds!"<<endl;
+}
