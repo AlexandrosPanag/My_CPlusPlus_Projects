ee2059a0999fbc60879124634c7a491ce472fcd8
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-07-11 06:41:15 +0300

    Add files via upload

---
+++ Functions/Dice Rolling Game.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+int rolldice(int dice){
+    int dice=0;
+    x = rand() % 6;
+    return dice;
+}
+
+
+int main(){
+cout<<"-----CREATED BY ALEXANDROS PANAGIOTAKOPOULOS-------------"<<endl;
+cout<<"Welcome to my dice rolling mini game"<<endl<<endl<<endl;
+
+
+
+while(true){
+    int dice=0;    
+    int diceresult=0;
+    cout<<"The banker game now roll the dice..."<<endl;
+    diceresult=rolldice(dice);
+    cout<<"The game rolled a %d",dice<<endl;
+    break;
+}
+
+}
