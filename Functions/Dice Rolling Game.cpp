9fb697151e79992d76260693cb6c8d451bc15524
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-07-13 03:14:56 +0300

    Update Dice Rolling Game.cpp

--- Functions/Dice Rolling Game.cpp
+++ Functions/Dice Rolling Game.cpp
@@ -17,7 +17,7 @@ cout<<"Welcome to my dice rolling mini game"<<endl<<endl<<endl;
 while(true){
     int dice=0;    
     int diceresult=0;
-    cout<<"The banker game now roll the dice..."<<endl;
+    cout<<"The banker will now roll the dice..."<<endl;
     diceresult=rolldice(dice);
     cout<<"The game rolled a %d",dice<<endl;
     break;
