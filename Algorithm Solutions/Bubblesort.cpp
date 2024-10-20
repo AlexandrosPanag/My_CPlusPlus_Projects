4de69388fb5afe89e5cb61d2b2b6523bc5bfbb85
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2024-10-20 23:55:29 +0300

    Add files via upload

---
+++ Algorithm Solutions/Bubblesort.cpp
@@ -0,0 +1,37 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// BubbleSort function
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Utility function to print the array
+void printArray(const vector<int>& arr) {
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = { 64, 34, 25, 12, 22, 11, 90 };
+    bubbleSort(arr);
+    cout << "Sorted array: ";
+    printArray(arr);
+    return 0;
+}
